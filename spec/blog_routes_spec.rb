a96836df3b07c31b47774982104c32ba3cf09121
Jayson Smith <github@nes.33mail.com>
2019-03-14 23:34:56 -0700

    First two blog path checks

--- spec/blog_routes_spec.rb
+++ spec/blog_routes_spec.rb
@@ -2,10 +2,22 @@ require 'faraday'
 require 'pry-byebug'
 
 describe "blog routes" do
-    describe "/blog" do
+    describe "/blog root" do
         it "redirects to cucumber.ghost.io/" do
             res = Faraday.get "http://localhost:9001/blog"
 
+            found = res.headers.dig('set-cookie').include?('cucumber.ghost.io')
+            
+            expect(found).to be true
+        end
+    end
+
+    describe "old blog url with date in path /blog/1111/11/blog-slug" do
+        it "301s to /blog/blog-slug" do
+            res = Faraday.get "http://localhost:9001/blog/2014/01/28/cukeup-2014"
+
+            expect(res.status).to eq(301)
+            expect(res.headers.dig('location')).to eq("http://localhost:9001/blog/cukeup-2014")
         end
     end
 end
