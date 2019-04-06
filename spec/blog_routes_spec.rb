3a607c0fc5fe87f82da3497ac62091e36e60bcb9
jaysonesmith <github@nes.33mail.com>
2019-04-06 10:52:37 -0700

    Fix include :sweatsmile:

--- spec/blog_routes_spec.rb
+++ spec/blog_routes_spec.rb
@@ -27,7 +27,7 @@ describe '/blog routes' do
 
       expect(res.status).to eq 301
       # to save money on the staging dyno, override the assertion to look for non-ssl routing here
-      if BASE_URL.includes('staging')
+      if BASE_URL.include?('staging')
         expect(res.headers.dig('location')).to eq("http://cucumber-io-proxy-staging.herokuapp.com/blog/cukeup-2014")
       else
         expect(res.headers.dig('location')).to eq("#{BASE_URL}/blog/cukeup-2014")
