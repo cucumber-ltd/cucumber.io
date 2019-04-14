e31e4d5d6ab6d97f0bb08e94556acba4b63d54b7
jaysonesmith <github@nes.33mail.com>
2019-04-13 22:08:12 -0700

    port

--- spec/squarespace_routes_spec.rb
+++ spec/squarespace_routes_spec.rb
@@ -16,7 +16,7 @@ describe 'squarespace routes' do
 
       expect(res.status).to eq 301
       if BASE_URL.include?('staging')
-        puts ENV
+        puts ENV["PORT"]
         expect(res.headers.dig('location')).to eq("https://cucumber-io-proxy-staging.herokuapp.com/events/2018/10/18/bdd-kickstart-austin")
       else
         expect(res.headers.dig('location')).to eq("#{BASE_URL}/events/2018/10/18/bdd-kickstart-austin")
