12962b795f5730fec04acf13b1b09efd344a64c0
jaysonesmith <github@nes.33mail.com>
2019-04-13 21:33:10 -0700

    Update nginx conf and add two tests

--- spec/squarespace_routes_spec.rb
+++ spec/squarespace_routes_spec.rb
@@ -9,4 +9,22 @@ describe 'squarespace routes' do
       expect(res.headers.dig('x-proxy-pass')).to eq 'https://cucumber-website.squarespace.com'
     end
   end
+
+  describe '/events/bdd-kickstart-austin-18' do
+    it 'redirects to posting the event\'s permanent page' do
+      res = Faraday.get "#{BASE_URL}/events/bdd-kickstart-austin-18"
+
+      expect(res.status).to eq 301
+      expect(res.headers.dig('location')).to eq("#{BASE_URL}/events/2018/10/18/bdd-kickstart-austin")
+    end
+  end
+
+  describe '/posting-rules.html' do
+    it 'redirects to posting rules in the support path' do
+      res = Faraday.get "#{BASE_URL}/posting-rules.html"
+
+      expect(res.status).to eq 301
+      expect(res.headers.dig('location')).to eq("#{BASE_URL}/support/posting-rules")
+    end
+  end
 end
