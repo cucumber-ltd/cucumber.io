19d868b6499bc07de9c8fb8723e3f8426cf8a858
jaysonesmith <github@nes.33mail.com>
2019-04-19 23:31:30 -0700

    Proxy squarespace images

--- spec/images_routes.rb
+++ spec/images_routes.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 describe 'images routes' do
-    describe 'an image in the /content directory' do
+    describe 'a ghost hosted image via the /content directory' do
       it 'proxies to the applicable ghost url' do
         res = Faraday.get "#{BASE_URL}/content/images/2019/02/cucumber-black-512.png"
   
         expect(res.status).to eq 200
         expect(res.headers.dig('x-proxy-pass')).to eq 'https://cucumber.ghost.io/content/images/2019/02/cucumber-black-512.png'
+      end
+    end
+
+    describe 'a squarespace hosted image in the /static directory' do
+      it 'proxies to the applicable squarespace url' do
+        res = Faraday.get "#{BASE_URL}/static/5b64cabf5b409bbf05dbd8b3/t/5b8fad55352f53909ed52c75/1536057610588/Cukenfest+2018-1681.jpg"
+  
+        expect(res.status).to eq 200
+        expect(res.headers.dig('x-proxy-pass')).to eq 'https://static1.squarespace.com/static/5b64cabf5b409bbf05dbd8b3/t/5b8fad55352f53909ed52c75/1536057610588/Cukenfest+2018-1681.jpg'
       end
     end
   end
