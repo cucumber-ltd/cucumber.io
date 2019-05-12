5cb0a0b07891b3343fd643275e037bfb9ebcbcb0
jaysonesmith <github@nes.33mail.com>
2019-05-11 21:54:52 -0700

    Check for needing to update

---
+++ spec/sitemap_generator/needs_update_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/sitemap_generator/update_checker.rb'
+
+describe 'Update Checker' do
+  ud = SitemapGenerator::UpdateChecker.new
+  describe 'needs update?' do
+    context 'the external sitemap\'s last modified is newer than our sitemap\'s last modified' do
+      it 'returns true' do
+        stub_request(:any, 'https://cucumber.ghost.io/sitemap.xml')
+          .to_return(headers: { 'last-modified' => 'Sun, 12 May 2019 04:59:14 GMT' })
+        stub_request(:any, 'https://cucumber.io/sitemap.xml')
+          .to_return(headers: { 'last-modified' => 'Sun, 01 May 2019 04:59:14 GMT' })
+
+        actual = ud.needs_update?('sitemap.xml')
+
+        expect(actual).to be true
+      end
+    end
+
+    context 'the external sitemap\'s last modified is older than our sitemap\'s last modified' do
+      it 'returns false' do
+        stub_request(:any, 'https://cucumber.ghost.io/sitemap.xml')
+          .to_return(headers: { 'last-modified' => 'Sun, 01 May 2019 04:59:14 GMT' })
+        stub_request(:any, 'https://cucumber.io/sitemap.xml')
+          .to_return(headers: { 'last-modified' => 'Sun, 10 May 2019 04:59:14 GMT' })
+
+        actual = ud.needs_update?('sitemap.xml')
+
+        expect(actual).to be false
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@
 
 require 'faraday'
 require 'pry-byebug'
+require 'webmock/rspec'
 
 BASE_URL = ENV['BASE_URL'] || 'http://localhost:9001'
