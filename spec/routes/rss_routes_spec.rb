6dbc48c8be7f89d5fa86b022e5f4bea686cad165
jaysonesmith <gh@nes.33mail.com>
2019-07-18 21:09:31 -0700

    Initial cut over to new pages

--- spec/routes/rss_routes_spec.rb
+++ spec/routes/rss_routes_spec.rb
@@ -9,4 +9,13 @@ describe 'rss routes' do
       expect(res.headers.dig('x-proxy-pass')).to eq 'https://cucumber.io/blog/rss.xml'
     end
   end
+
+  describe '/feed.xml' do
+    it 'returns the current version on S3' do
+      res = Faraday.get "#{BASE_URL}/feed.xml"
+
+      expect(res.status).to eq 200
+      expect(res.headers.dig('x-proxy-pass')).to eq 'https://cucumber.io/blog/rss.xml'
+    end
+  end
 end
