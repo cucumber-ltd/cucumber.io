5177d21a9750525579734a0132dcb6be829b933e
jaysonesmith <gh@nes.33mail.com>
2019-07-27 23:41:03 -0700

    Remove code check

--- spec/routes/static_file_routes_spec.rb
+++ spec/routes/static_file_routes_spec.rb
@@ -14,7 +14,6 @@ describe 'images routes' do
     it 'proxies to the applicable squarespace url' do
       res = Faraday.get "#{BASE_URL}/static/5b64cabf5b409bbf05dbd8b3/t/5b8fad55352f53909ed52c75/1536057610588/Cukenfest+2018-1681.jpg"
 
-      expect(res.status).to eq 301
       expect(res.headers.dig('x-proxy-pass')).to eq 'https://static1.squarespace.com/static/5b64cabf5b409bbf05dbd8b3/t/5b8fad55352f53909ed52c75/1536057610588/Cukenfest+2018-1681.jpg'
     end
   end
