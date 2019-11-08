9436fdbbf7dc0acf2e2c5e43358be2700a029401
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-11-08 21:13:45 +0000

    Whitespace

--- spec/routes/sponsor_routes_spec.rb
+++ spec/routes/sponsor_routes_spec.rb
@@ -13,7 +13,7 @@ describe 'sponsor routes' do
   describe '/sponsors/' do
     it 'proxies to https://cucumber.netlify.com/sponsors' do
       res = Faraday.get "#{BASE_URL}/sponsors/"
-  
+
       expect(res.status).to eq 200
       expect(res.headers.dig('x-proxy-pass')).to eq 'https://cucumber.netlify.com/sponsors/'
     end
